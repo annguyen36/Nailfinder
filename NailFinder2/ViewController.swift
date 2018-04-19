c38bd202d0da0551b329ed0386ba9bf63d42fd98
Ân Nguyễn <nguyenvuthienan36@gmail.com>
2018-04-19 14:30:07 -0500

    SignUpLogin

--- NailFinder2/ViewController.swift
+++ NailFinder2/ViewController.swift
@@ -7,19 +7,80 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var driverLabel: UILabel!
+    @IBOutlet weak var riderLabel: UILabel!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var riderDriverSwitch: UISwitch!
+    @IBOutlet weak var topBtn: UIButton!
+    @IBOutlet weak var bottomBtn: UIButton!
+    
+    var signUpMode = true
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
 
+    @IBAction func topTapped(_ sender: Any) {
+        if emailTextField.text == "" || passwordTextField.text == "" {
+            displayAlert(title: "Missing Information", message: "You must provide email and Password")
+        } else {
+            if let email = emailTextField.text {
+                if let password = passwordTextField.text {
+                    if signUpMode {
+                        //Sign Up
+                        Auth.auth().createUser(withEmail: email, password: password) { (user, error) in
+                            if error != nil {
+                                self.displayAlert(title: "Error", message: error!.localizedDescription)
+                            } else {
+                                print("Sign up Success")
+                               // self.performSegue(withIdentifier: "chuSegue", sender: nil)
+                            }
+                        }
+                        
+                    } else {
+                        //Log in
+                        Auth.auth().signIn(withEmail: email, password: password) { (user, error) in
+                            if error != nil {
+                                self.displayAlert(title: "Error", message: error!.localizedDescription)
+                            } else {
+                                print("log in Success")
+                               // self.performSegue(withIdentifier: "chuSegue", sender: nil)
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+    func displayAlert(title:String, message:String){
+        let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        alertController.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        self.present(alertController, animated: true, completion: nil)
+    }
+    @IBAction func bottomTapped(_ sender: Any) {
+        if signUpMode {
+            topBtn.setTitle("Log In", for: .normal)
+            bottomBtn.setTitle("Switch To Sign Up", for: .normal)
+            driverLabel.isHidden = true
+            riderLabel.isHidden = true
+            riderDriverSwitch.isHidden = true
+            signUpMode = false
+        } else {
+            topBtn.setTitle("Sign Up", for: .normal)
+            bottomBtn.setTitle("Switch To Log in", for: .normal)
+            driverLabel.isHidden = false
+            riderLabel.isHidden = false
+            riderDriverSwitch.isHidden = false
+            signUpMode = true
+        }
+    }
+    
 
 }
 
