0c4296a1df335c8f61cea0e182faedf4264d578c
Ân Nguyễn <nguyenvuthienan36@gmail.com>
2018-04-19 13:11:10 -0500

    Initial Commit

---
+++ NailFinder2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NailFinder2
+//
+//  Created by An Nguyen on 4/19/18.
+//  Copyright © 2018 An Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
